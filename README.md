e043041444fd4c60f489f607de7cec7ff9da1b28
Mikkel Hoegh <mikkel@hoegh.org>
2013-06-18 13:36:11 +0200

    Initial version of json_test module.

---
+++ README.md
@@ -0,0 +1,4 @@
+JSON test module
+================
+
+Test case for [this issue on Drupal.org](https://drupal.org/node/341588).
---
+++ json_test.js
@@ -0,0 +1,20 @@
+/**
+ * @file
+ * Test JavaScript for the JSON test module.
+ */
+
+jQuery(function($) {
+  "use strict";
+
+  var placeholder = $('.json-test-placeholder');
+
+  $.getJSON(Drupal.settings.basePath + 'json_test.json', function (response) {
+    if (response && response.test) {
+      placeholder.text(response.test);
+    }
+    else {
+      placeholder.addClass('error');
+      placeholder.text('Failed to load.');
+    }
+  });
+});
